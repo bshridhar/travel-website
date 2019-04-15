197795326b192b1d2f2605d9a56e03206e72bde1
bshridhar <shridhar.baddur@gmail.com>
2019-04-15 15:00:10 +0530

    styling the site with PostCSS

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimports = require('postcss-import');
+cssimports = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(done) {
     console.log("Imagine SASS or PostCSS tasks running here!");
     return  gulp.src('./app/assets/styles/styles.css')
-            .pipe(postcss([cssimports, nested, cssvars, autoprefixer]))
+            .pipe(postcss([cssimports, mixins, nested, cssvars, autoprefixer]))
             .on('error', function(errorInfo) {
                 console.log(errorInfo.toString());
                 this.emit('end');
