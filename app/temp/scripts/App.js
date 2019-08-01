dd02be33f81514238904255f62e2cdf24b634c21
bshridhar <shridhar.baddur@gmail.com>
2019-08-01 15:17:01 +0530

    completed reveal on scroll behaviour

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -46,65 +46,26 @@
 
 	'use strict';
 
-	var _MobileMenu = __webpack_require__(1);
+	var _jquery = __webpack_require__(1);
+
+	var _jquery2 = _interopRequireDefault(_jquery);
+
+	var _MobileMenu = __webpack_require__(2);
 
 	var _MobileMenu2 = _interopRequireDefault(_MobileMenu);
+
+	var _RevealOnScroll = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./modules/RevealOnScroll\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+	var _RevealOnScroll2 = _interopRequireDefault(_RevealOnScroll);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	var mobileMenu = new _MobileMenu2.default();
+	new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
+	new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "60%");
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	    value: true
-	});
-
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _jquery = __webpack_require__(2);
-
-	var _jquery2 = _interopRequireDefault(_jquery);
-
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	var MobileMenu = function () {
-	    function MobileMenu() {
-	        _classCallCheck(this, MobileMenu);
-
-	        this.siteHeader = (0, _jquery2.default)('.site-header');
-	        this.menuIcon = (0, _jquery2.default)('.site-header__menu-icon');
-	        this.menuContent = (0, _jquery2.default)('.site-header__menu-content');
-	        this.events();
-	    }
-
-	    _createClass(MobileMenu, [{
-	        key: 'events',
-	        value: function events() {
-	            this.menuIcon.click(this.toggleTheMenu.bind(this));
-	        }
-	    }, {
-	        key: 'toggleTheMenu',
-	        value: function toggleTheMenu() {
-	            this.menuContent.toggleClass('site-header__menu-content--is-visible');
-	            this.siteHeader.toggleClass('site-header--is-expanded');
-	            this.menuIcon.toggleClass('site-header__menu-icon--close-x');
-	        }
-	    }]);
-
-	    return MobileMenu;
-	}();
-
-	exports.default = MobileMenu;
-
-/***/ }),
-/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -9922,6 +9883,55 @@
 	return jQuery;
 	}));
 
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _jquery = __webpack_require__(1);
+
+	var _jquery2 = _interopRequireDefault(_jquery);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	var MobileMenu = function () {
+	    function MobileMenu() {
+	        _classCallCheck(this, MobileMenu);
+
+	        this.siteHeader = (0, _jquery2.default)('.site-header');
+	        this.menuIcon = (0, _jquery2.default)('.site-header__menu-icon');
+	        this.menuContent = (0, _jquery2.default)('.site-header__menu-content');
+	        this.events();
+	    }
+
+	    _createClass(MobileMenu, [{
+	        key: 'events',
+	        value: function events() {
+	            this.menuIcon.click(this.toggleTheMenu.bind(this));
+	        }
+	    }, {
+	        key: 'toggleTheMenu',
+	        value: function toggleTheMenu() {
+	            this.menuContent.toggleClass('site-header__menu-content--is-visible');
+	            this.siteHeader.toggleClass('site-header--is-expanded');
+	            this.menuIcon.toggleClass('site-header__menu-icon--close-x');
+	        }
+	    }]);
+
+	    return MobileMenu;
+	}();
+
+	exports.default = MobileMenu;
 
 /***/ })
 /******/ ]);
