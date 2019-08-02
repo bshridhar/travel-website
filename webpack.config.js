b39ac05a6c215cdb4edf2977e4583159cb2dc271
bshridhar <shridhar.baddur@gmail.com>
2019-08-02 17:41:51 +0530

    lazy loading completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var webpack = require('webpack');
-var ignore = new webpack.IgnorePlugin(new RegExp("/(node_modules|waypoints)/"))
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: "./app/temp/scripts",
-    filename: "App.js"
+    filename: "[name].js"
   },
   module: {
     loaders: [
@@ -17,8 +18,5 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  },
-  plugins: [
-    ignore,
-  ]
+  }
 }
